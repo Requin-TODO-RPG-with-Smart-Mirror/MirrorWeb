66da41ac9752b7f237a34226b58b22d8e9e64d1c
LeagueLugas <blackblack9685@gmail.com>
2019-11-26 21:19:45 +0900

    To do list 페이지 UI 완성

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function changeMenu(tag, title, page) {
+	document.getElementById('headerTitle').innerHTML = title;
+	
+	let list = document.getElementsByClassName('footer_menu_img');
+	for(var i = 0; i < list.length; i++) {
+		list[i].style['opacity'] = '0.7';
+		list[i].style = 'transform: scale(1)';
+	}
+	
+	let pageList = document.getElementsByClassName('mainMenuList');
+	for(var i = 0; i < pageList.length; i++) {
+		pageList[i].style = 'transform: translate('+(-100*page)+'%,0)';
+	}
+	
+	tag.style['opacity'] = '1.0';
+	tag.style = 'transform: scale(1.25)';
+}
